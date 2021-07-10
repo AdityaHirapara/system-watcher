186cf57aaaaacd557b4826d94d84848f17224a41
Aditya Hirapara <adityahirapara2016@gmail.com>
2021-07-10 23:20:18 +0530

    Implement basic tray application:seeding:

--- main.go
+++ main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+
+	systray "github.com/getlantern/systray"
 )
 
+var memUsage float64 = 0
+
 func main() {
-	fmt.Println("Welcome to the Goland!")
+	systray.Run(onReady, onExit)
+}
+
+func onReady() {
+	systray.SetTitle("💹")
+	systray.SetTooltip("System Watcher")
+	titleItem := systray.AddMenuItem("System Watcher", "")
+
+	ramStatusItem := systray.AddMenuItem(fmt.Sprintf("Memory usage: %.2f%%", memUsage), "")
+
+	systray.AddSeparator()
+	quitItem := systray.AddMenuItem("Quit", "")
+
+	titleItem.Disable()
+	ramStatusItem.Disable()
+
+	go func() {
+		for {
+			select {
+			case <-quitItem.ClickedCh:
+				systray.Quit()
+				return
+			}
+		}
+	}()
+}
+
+func onExit() {
+	fmt.Println("Exited")
 }
